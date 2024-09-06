20aece93ae99ddcab8af446764bf8f48607a2320
dev-sanjaya <sanjaya.theoceann@gmail.com>
2024-09-06 16:21:54 +0530

    models

--- model.py
+++ model.py
@@ -63,7 +63,7 @@ def run_spacy_train(config_path, output_path, train_data_path, dev_data_path):
 
 
 if __name__ == "__main__":
-    models = ['tonnage_info']
+    models = ['cargo']
 
     for model in models:
         print(f'Starting process for model: {model}')
