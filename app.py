be38be90791e877e51b704f70e93cf0be01743eb
dev-sanjaya <sanjaya.theoceann@gmail.com>
2024-09-09 14:59:48 +0530

    models updated

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import uvicorn
 import json
 import spacy
 from pathlib import Path
@@ -7,22 +8,18 @@ from fastapi.middleware.gzip import GZipMiddleware
 from datetime import datetime, timedelta
 import asyncio
 
-# Initialize FastAPI app
 app = FastAPI()
 
-# Add GZip middleware for compression
 app.add_middleware(GZipMiddleware, minimum_size=1000)
 
-# Function to get the current time in IST format
 def get_ist_time():
     utc_time = datetime.utcnow()
     ist_time = utc_time + timedelta(hours=5, minutes=30)
     return ist_time.strftime('%Y-%m-%d %H:%M:%S')
 
-# JSON file to store API statistics
+
 stats_file_path = Path('api_status.json')
 
-# Initialize JSON stats file if it doesn't exist
 if not stats_file_path.exists():
     initial_data = {
         'timestamp': {'timestamp': get_ist_time()},
@@ -32,7 +29,6 @@ if not stats_file_path.exists():
     with open(stats_file_path, 'w') as f:
         json.dump(initial_data, f, indent=4)
 
-# Function to update API stats for each API call
 def update_api_stats(api_name):
     with open(stats_file_path, 'r+') as f:
         data = json.load(f)
@@ -42,33 +38,28 @@ def update_api_stats(api_name):
         json.dump(data, f, indent=4)
         f.truncate()
 
-# Load NLP models for vessel_info, tonnage_info, and cargo
 vessel_nlp = spacy.load(Path(f"models/vessel_info/model-best"))
 tonnage_nlp = spacy.load(Path(f"models/tonnage_info/model-best"))
 cargo_nlp = spacy.load(Path(f"models/cargo/model-best"))
 
-# Home route to display the API statistics
 @app.get("/")
 async def home():
     with open(stats_file_path, 'r') as f:
         data = json.load(f)
     return JSONResponse(content=data)
 
-# POST endpoint for vessel_info and tonnage_info predictions
 @app.post("/predict/tonnage")
 async def predict_vessel_and_tonnage(request: Request):
     update_api_stats('tonnage')
     data = await request.json()
     return await predict_combined([vessel_nlp, tonnage_nlp], data)
 
-# POST endpoint for cargo predictions
 @app.post("/predict/cargo")
 async def predict_cargo(request: Request):
     update_api_stats('cargo')
     data = await request.json()
     return await predict_combined([cargo_nlp], data)
 
-# Function to combine predictions from multiple models
 async def predict_combined(models, request_data):
     if 'text' not in request_data:
         raise HTTPException(status_code=400, detail="No text provided")
@@ -86,7 +77,5 @@ async def predict_combined(models, request_data):
 
     return JSONResponse(content={"entities": combined_result})
 
-# Entry point for running the app with Uvicorn
 if __name__ == '__main__':
-    import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000, reload=True, workers=4)
