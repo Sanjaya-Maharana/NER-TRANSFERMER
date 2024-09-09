fbe922a22ada3da9509859129b2a8e4b5e81c60a
dev-sanjaya <sanjaya.theoceann@gmail.com>
2024-09-09 15:25:09 +0530

    models updated

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import json
 import spacy
 from pathlib import Path
 from fastapi import FastAPI, Request, HTTPException
-from fastapi.responses import JSONResponse
 from fastapi.middleware.gzip import GZipMiddleware
 from datetime import datetime, timedelta
 import asyncio
