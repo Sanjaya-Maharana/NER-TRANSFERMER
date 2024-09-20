46ff841c403980662b35997c3ecd87d433a1f324
dev-sanjaya <sanjaya.theoceann@gmail.com>
2024-09-20 17:34:31 +0530

    models updated

--- app.py
+++ app.py
@@ -6,51 +6,10 @@ from fastapi import FastAPI, Request, HTTPException
 from fastapi.middleware.gzip import GZipMiddleware
 from datetime import datetime, timedelta
 import asyncio
-import subprocess
-import platform
 
 app = FastAPI()
 
 app.add_middleware(GZipMiddleware, minimum_size=1000)
-
-
-def check_and_install_java():
-    try:
-        java_version = subprocess.run(['java', '-version'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        if java_version.returncode == 0:
-            version_info = java_version.stderr.decode().split('\n')[0]
-            return {"message": "Java is already installed", "java_version": version_info}
-
-    except FileNotFoundError:
-        try:
-            distro = platform.linux_distribution()[0].lower()
-
-            if 'ubuntu' in distro or 'debian' in distro:
-                print("Installing Java on Ubuntu/Debian...")
-                subprocess.run(['sudo', 'apt', 'update'], check=True)
-                subprocess.run(['sudo', 'apt', 'install', '-y', 'default-jre'], check=True)
-
-            elif 'centos' in distro or 'fedora' in distro or 'rhel' in distro:
-                print("Installing Java on CentOS/Fedora/RHEL...")
-                subprocess.run(['sudo', 'yum', 'install', '-y', 'java-11-openjdk'], check=True)
-
-            else:
-                return {"message": "Unsupported Linux distribution. Please install Java manually."}
-
-            java_version = subprocess.run(['java', '-version'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            if java_version.returncode == 0:
-                version_info = java_version.stderr.decode().split('\n')[0]
-                return {"message": "Java installed successfully", "java_version": version_info}
-
-        except subprocess.CalledProcessError as e:
-            return {"error": f"An error occurred during installation: {e}"}
-
-    except Exception as ex:
-        return {"error": f"Unexpected error: {ex}"}
-
-@app.get("/install_java")
-def install_java():
-    return check_and_install_java()
 
 
 def get_ist_time():
@@ -123,47 +82,5 @@ async def predict_combined(models, request_data):
     return {"entities": combined_result}
 
 
-@app.post("/check_text")
-async def check_text(request: Request):
-    import language_tool_python
-    tool = language_tool_python.LanguageTool('en-US')
-    data = await request.json()
-    text = data.get('text', '')
-    matches = tool.check(text)
-    incorrect_words = []
-    corrected_words = []
-    corrected_sentence = list(text)
-
-    try:
-        offset_correction = 0
-        for match in matches:
-            incorrect_word = text[match.offset:match.offset + match.errorLength]
-            suggestion = match.replacements[0] if match.replacements else incorrect_word
-
-            incorrect_words.append(incorrect_word)
-            corrected_words.append(suggestion)
-
-            corrected_sentence[
-            match.offset + offset_correction:match.offset + match.errorLength + offset_correction] = suggestion
-
-            offset_correction += len(suggestion) - len(incorrect_word)
-
-        corrected_sentence = ''.join(corrected_sentence)
-
-        return {
-            'incorrect_words': incorrect_words,
-            'corrected_words': corrected_words,
-            'corrected_sentence': corrected_sentence
-        }
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return {
-            'incorrect_words': incorrect_words,
-            'corrected_words': corrected_words,
-            'corrected_sentence': ''.join(corrected_sentence),
-            'error': str(e)
-        }
-
-
 if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=8000, loop="uvloop", http="httptools", workers=4)
+    uvicorn.run(app, host="0.0.0.0", port=6000, loop="uvloop", http="httptools", workers=4)
