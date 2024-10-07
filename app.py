eb6aa2b1096547b1de9a67700819bd383fdc31b0
dev-sanjaya <sanjaya.theoceann@gmail.com>
2024-10-07 18:18:00 +0530

    models updated

--- app.py
+++ app.py
@@ -17,6 +17,12 @@ import jwt
 from jwt.exceptions import ExpiredSignatureError, InvalidTokenError
 from datetime import datetime
 from googletrans import Translator
+import openai
+
+openai.api_type = "azure"
+openai.api_base = "https://extractinfo.openai.azure.com/"
+openai.api_version = "2023-07-01-preview"
+openai.api_key = "30363b3002684528a6af160e7cb7ae31"
 
 JWT_USER_SECRET_KEY = "asdgcvsdcv@@@#$@%@!~!~!!)(U*@*fdbvjblejhfvhgvsjfgv$@%&*(W&!)W(!SDHQWFUWKDDOY@TEF@&ETO!*E@(T@(ET!QDXWFBCWJWFGEKUFEUE"
 ADMIN_SECRET_JWT_TOKEN = "rtawdchvscfbdhfvbjkdfnvhdgfjhhHHHHH@@!$@#(%*#$@(*)#!()@*$73y8277"
@@ -164,15 +170,40 @@ async def predict_cargo(request: Request):
     data = await request.json()
     return await predict_combined([cargo_nlp], data)
 
-async def detect_and_translate(data):
+async def detect_and_translate_html(content):
     text = data['text']
     if not text.strip():
         raise HTTPException(status_code=400, detail="No text provided")
-    detected_lang = translator.detect(text)
-    if detected_lang.lang != 'en':
-        translation = translator.translate(text, dest='en')
-        return {"translated_text": translation.text}
-    else:
+    try:
+        response = openai.ChatCompletion.create(
+            engine="gpt-4",
+            messages=[
+                {
+                    "role": "system",
+                    "content": (
+                        "You are an AI assistant that detects and translates the text to English while preserving the HTML structure. "
+                        "Translate any non-English text within the HTML content but ensure the HTML tags remain unchanged."
+                        "Response must have only html no other text"
+                    )
+                },
+                {
+                    "role": "user",
+                    "content": f"Here is the HTML content to translate:\n\n{text}"
+                }
+            ],
+            temperature=0.2,
+            top_p=0.95,
+            frequency_penalty=0,
+            presence_penalty=0,
+            stop=None
+        )
+
+        if response and "choices" in response and response["choices"]:
+            translated_content = response["choices"][0]["message"]["content"]
+            return {"translated_text": translated_content}
+        else:
+            return {"translated_text": text}
+    except Exception as e:
         return {"translated_text": text}
 
 async def predict_combined(models, request_data):
@@ -339,7 +370,7 @@ async def plot_data(request_data: PlotDataRequest):
 async def predict_vessel_and_tonnage(request: Request):
     update_api_stats('translate')
     data = await request.json()
-    return await detect_and_translate(data)
+    return await detect_and_translate_html(data)
 
 
 if __name__ == '__main__':
