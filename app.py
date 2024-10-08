3e67c25e171e722580f5ff7d5cbd7f194c2909af
dev-sanjaya <sanjaya.theoceann@gmail.com>
2024-10-08 09:56:48 +0530

    models updated

--- app.py
+++ app.py
@@ -39,8 +39,7 @@ app.add_middleware(
     allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
-    max_body_size=10 * 1024 * 1024  # 10 MB
+    allow_headers=["*"]
 )
 
 app.add_middleware(GZipMiddleware, minimum_size=1000)
